ab33c161943fbaf100ea29bee395ba53eb308d9b
absolut0 <36243693+absolut0@users.noreply.github.com>
2024-07-09 16:27:24 -0400

    June 2024 build

--- app/(full-page)/starthere/page.tsx
+++ app/(full-page)/starthere/page.tsx
@@ -45,7 +45,9 @@ const MiscDemo = () => {
             window.scrollTo(0, 0);
           }
       };
-
+      const handleRestart = () => {
+        location.reload();
+      };
       const YoutubeEmbed = ({ embedId }) => (
 /*        <div className="video-responsive">
           <iframe
@@ -149,27 +151,45 @@ const MiscDemo = () => {
             >
                 
                 <div>
-                {show === 1 && (<div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
-                    <h2>Getting Started</h2>
-                    <p>The following is a series of steps meant to guide you through in-processing. Complete the steps that
-                        you can, not all steps need to be completed. Some may apply to your specific situation but some may 
-                        not. This system is designed to assist those individuals who do not have the necessary credentials 
-                        yet to log into a NIPR connected machine just yet. </p>
-                        <p>
-                        With your help we can complete the remaining in-processing steps quickly and get you up and running 
-                        with minimal disruptions. This system is not meant to house PII, any forms that contain PII should
-                        not be stored here. If you need any assistance please contact the CIO Team.
-                        </p>
-                </div>
+                {show === 1 && (
+                    <div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
+                        <h2>Getting Started</h2>
+                        <p>The following is a series of steps meant to guide you through in-processing. Complete the steps that
+                            you can, not all steps need to be completed. Some may apply to your specific situation but some may 
+                            not. This system is designed to assist those individuals who do not have the necessary credentials 
+                            yet to log into a NIPR connected machine just yet. </p>
+                            <p>
+                            With your help we can complete the remaining in-processing steps quickly and get you up and running 
+                            with minimal disruptions. This system is not meant to house PII, any forms that contain PII should
+                            not be stored here. If you need any assistance please contact the CIO Team.
+                            </p>
+                    </div>
                 
                 )} 
-                {show === 2 && (
+                {show === 2 && 
+                (
+                    <div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
+                        
+                        <div style={{maxWidth: "fit-content", marginInline:"auto"}}><img srcSet={`/img/PEOLogo.png` } style={{width:"200px", marginLeft:"40px"}}/>
+                        <h1>PEO Soldier HQ</h1></div>
+                        <p style={{textAlign:"center"}}><h3>These procedures were developed to communicate and facilitate the required steps to equip you to be successful and productive.
+                        </h3>
+                        </p>
+                            <p style={{textAlign:"center"}}>
+                            <h4>We will work together to help facilitate your smooth and successful integration into our team!</h4>
+                            </p>
+                    </div>
+                
+                )
+                /*(
                 <div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
                     <div className="col-12 peos-slideImg" >
                         <img srcSet={`/img/Intro.jpg 1980w`}/>
                     </div>                    
                 </div>
-                )}                         
+                )*/
+                
+                }                         
                 {show === 3 && (<div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
                     <h2>About PEO Soldier</h2>
                     
@@ -222,9 +242,37 @@ const MiscDemo = () => {
                  {show === 5 && (
                 <div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
                     <h2>Common Access Card (CAC) Retrieval</h2>
-                    <div className="col-12 peos-slideImg" >
+                    {/*<div className="col-12 peos-slideImg" >
                         <img srcSet={`/img/CAC.jpg 320w, /img/CAC.jpg 680w, /img/CAC.jpg  960w, /img/CAC.jpg 1980w`}/>
-                    </div>                    
+                    </div>     */}
+                    <p>
+                        <h4>New DA Civilian Personnel: </h4>
+                        PEO Soldier HQ Civilian Personnel Office will notify you when you can get your CAC 
+                        once your personnel action has processed.
+
+                        <h4>Military Members:</h4>
+                        If applicable, PEO Soldier HQ Military Personnel Office will notify you 
+                        when you get your CAC.
+
+                        <h4>Contractors:</h4>
+                        You will receive an e-mail to proceed to CAC issue point if your choosing once 
+                        your CAC application has been processed.  
+
+                        <h4>How to Get Your CAC:</h4>
+                        <ul><li>Walk into Fort Belvoir DEERS Office (
+                            <a href="https://home.army.mil/belvoir/about/Garrison/directorate-human-resources/military-personnel-division/deersid-cards" target="_blank">Link to DEERS Information</a>)
+                        </li>
+                        <li>
+                            Schedule appointment using the 
+                            <a href="https://idco.dmdc.osd.mil/idco/" target="_blank"> RAPIDS Appointment Scheduler</a>
+                        </li>
+                        </ul>
+                        <p>If you receive a CAC with no e-mail address attached to it (you can ask the DEERS personnel if they see 
+                        your e-mail address in the system and what it is), you must call AESD (866-335-2769) to ask for the e-mail 
+                        address that was created an assigned to your CAC.
+                        </p>
+                        <h3>Two forms if ID required to get CAC!</h3>
+                    </p>               
                 </div>
                 )}      
                 {show === 6 && (
@@ -243,9 +291,30 @@ const MiscDemo = () => {
                  {show === 7 && (
                 <div className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
                     <h2>IT Equipment</h2>
-                    <div className="col-12 peos-slideImg" >
+                    <p><ul>
+                    <li>Once steps the previous steps have been completed, the PEO HQ IT Team will contact you 
+                        to coordinate a time for equipment pick up or shipment</li>
+                    <li>Distro List Updates - The Linked Account in your IO Profile will be updated. </li>
+                    <li>Your group membership will be updated to grant you access to online access
+                            to SharePoint and relevant applications</li>
+                    </ul>
+                    </p>
+                    <p>Typical Government Furnished Equipment (GFE) furnished:
+                        <ul>
+                        <li>Laptop</li>
+                        <li>Monitors (for cubicle and/or teleworking)</li>
+                        <li>Keyboard</li>
+                        <li>Mouse</li>
+                        <li>Government Cell Phone (iPhone) - if approved
+                            <ul><li>All Government iPhones must be approved by the PEO HQ Entitlement
+                        Manager (Mike Laiacona)</li></ul></li>
+                        </ul>
+                        <h4>If approved, an IT Specialist will issue your devices to you</h4>
+                    </p>
+                    <h4>You will be provided a DA Form 2062, Hand Receipt, for all GFE issued to you that you must sign acknowledging receipt of the equipment</h4>
+                    {/*<div className="col-12 peos-slideImg" >
                         <img srcSet={`/img/Equipment.jpg 320w, /img/Equipment.jpg 680w, /img/Equipment.jpg 960w, /img/Equipment.jpg 1980w`}/>
-                    </div>                    
+                    </div>        */}            
                 </div>
                 )}                 
                 {show === 8 && (
@@ -266,11 +335,23 @@ const MiscDemo = () => {
                 {show === 9 && (
                 <div  className="card" style={{backgroundColor: "rgba(200,200,200,.8)"}}>
                     <h2>Personnel Accountability Reporting</h2>
+                    
+                    <ul>
+                        <p><li>All Personnel who have a NIPR account and/or a 0365 License from PEO Soldier
+                        MUST report their status in PAR daily NLT 0930 local time
+                            <ul><li>Matrix employees who report their daily status using another system with their parent organization are exempt from using PAR</li></ul>
+                        </li></p>
+                        <p><li>Personnel must complete and maintain/update their profile information to include emergency contact information</li></p>
+                        <p><li>Personnel can and should enter a future roll call status when they know their status for a future period will be other than at duty station or telework</li></p>
+                        <p><li>If you have trouble accessing PAR, contact your supervisor or PAR Admin
+                            <ul><li>PEO HQ PAR Admin is Nidhi Yadav</li></ul>
+                        </li></p>               
+                    </ul>
 
-                    <div className="peos-slideImg" >
+                    {/*<div className="peos-slideImg" >
                         <img srcSet={`/img/PAR.jpg 320w, /img/PAR.jpg 680w, /img/PAR.jpg  960w, /img/PAR.jpg 1980w`}/>
                         
-                    </div>  
+                    </div>  */}
   
                     
                 </div>
@@ -281,6 +362,9 @@ const MiscDemo = () => {
                     <p>You have reached the end of this guide. Hopefully you found this information useful. Remember to transfer any forms you have completed to the designated POC. 
                         Refresh this page to start over.
                     </p>
+                    <div className="col-12" style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Button type="button" label="Restart" style={{ margin: '10px'}} onClick={handleRestart}></Button>
+                    </div>
                 </div>     
                 )}
                 </div>     
